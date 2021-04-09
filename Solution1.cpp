827ff11f27634c049d7499a2ea5f097de17818b4
Radhika Dhaipule <radhika.dhaipule@gmail.com>
2021-04-08 22:25:29 -0400

    Working solution

--- Solution1.cpp
+++ Solution1.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        
-        if (nums.size()==0)
-            len = 0;
-        else if (nums.size()==1)
-            len = 1;
-        else
-        {
-            len=1;
-            for (auto it1 = nums.begin(); it1!=nums.end()-1; it1++)
-            { 
-                for (auto it2 = it1+1; it2!=nums.end(); it2++)
-                {
-                    temp = *it1;
-                        if (*it2 > temp)
-                            {
-                                temp = *it2;
-                                *it2 = *(it1+1);
-                                *(it1+1) = temp;
-                                len++; //problem length fails for test case 112344
-                            } 
-                    }
-            }
+  int removeDuplicates(vector<int> &nums) {
+
+    if (nums.size() == 0) {
+      len = 0;
+      return len;
+    } else if (nums.size() == 1) {
+      len = 1;
+      return len;
+    } else {
+      len = 1;
+      for (auto it1 = nums.begin(); it1 != nums.end() - 1; it1++) {
+        base = *it1;
+        for (auto it2 = it1 + 1; it2 != nums.end(); it2++) {
+          if ((*it2 == base) or (*it2 < base)) {
+          } else {
+            temp = *it2;
+            *it2 = *(it1 + 1);
+            *(it1 + 1) = temp;
+            break;
+          }
         }
-             
-        return len;
-        
+      }
     }
-    
-    int len;
-    int temp = 0;
-    vector<int>::iterator it2;
-    
+
+    for (auto it1 = nums.begin(); it1 != nums.end() - 1; it1++) {
+      if ((*it1 == *(it1 + 1)) or (*it1 > *(it1 + 1)))
+        return len;
+      else
+        len++;
+    }
+
+    return len;
+  }
+
+  int len;
+  int temp = 0;
+  int base;
 };
