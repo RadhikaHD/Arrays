11b6efb79b3d8066cb11673f55b8ee4caf0e8e5a
Radhika Dhaipule <radhika.dhaipule@gmail.com>
2021-04-17 15:55:38 -0400

    move zeroes using in place

--- Solution9.cpp
+++ Solution9.cpp
@@ -4,18 +4,20 @@ public:
     vector<int> copyvector;
     int len = nums.size();
     int countzeroes = 0;
+    int temp;
 
     for (int i = 0; i < len; i++) {
-      if (nums[i] == 0)
-        countzeroes++;
-      else
-        copyvector.push_back(nums[i]);
+      if (nums[i] == 0) {
+        // find the first non zero element from i+1 to len
+        for (int j = i + 1; j < len; j++) {
+          if (nums[j] != 0) {
+            temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+            break;
+          }
+        }
+      }
     }
-
-    for (int i = 0; i < countzeroes; i++) {
-      copyvector.push_back(0);
-    }
-
-    std::copy(copyvector.begin(), copyvector.end(), nums.begin());
   }
 };
